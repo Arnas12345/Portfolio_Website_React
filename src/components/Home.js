5e69db524a57deebabd2243247789bc4a2140459
Arnas12345 <arnasjuravicius12345@gmail.com>
2022-04-12 23:13:11 +0100

    CSS Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className="Page">
       <Particle />
-      <div className="row IntoText" style={{ height: "95vh", marginRight: 0 }}>
-        <div className="MainText col-md-7">
+      <div className="row IntroText">
+        <div className="MainText col-md-7" style={{ marginTop: 150 }}>
           <h1 className="Intro">Hi There.</h1>
           <h1 className="Intro">
             I'm <strong className="Name">Arnas Juravicius.</strong>
@@ -24,22 +24,30 @@ export default function Home() {
         </div>
       </div>
       <div>
-        <h1 className="Intro">
+        <h1 className="Intro" style={{ marginBottom: 25 }}>
           A LITTLE <strong className="Name">ABOUT ME</strong>
         </h1>
         <div className="AboutMeText">
           <p className="AboutMeFont">
-            I am a recent graduate from the University of Limerick where I
-            obtained a First Class Honors in a Bachelor of Science in Computer
-            Systems.
+            I am a recent <strong className="Name">graduate</strong> from the{" "}
+            <strong className="Name">University of Limerick</strong> where I
+            obtained a <strong className="Name">First Class Honors</strong> in a{" "}
+            <strong className="Name">
+              Bachelor of Science in Computer Systems.
+            </strong>
           </p>
           <p className="AboutMeFont">
-            I am of Lithuanian descent and have spent all my life in Ireland
-            therefore I am fluent in both English and Lithuanian.
+            I am of <strong className="Name">Lithuanian</strong> descent and
+            have grown up in <strong className="Name">Ireland</strong> therefore
+            I am <strong className="Name">fluent</strong> in both{" "}
+            <strong className="Name">English</strong> and{" "}
+            <strong className="Name">Lithuanian.</strong>
           </p>
-          <p className="AboutMeFont" style={{ marginBottom: 50 }}>
-            I am currently seeking to relocate to Barcelona in search of a
-            software engineering role.
+          <p className="AboutMeFont" style={{ marginBottom: 70 }}>
+            I am currently <strong className="Name">seeking</strong> to{" "}
+            <strong className="Name">relocate</strong> to{" "}
+            <strong className="Name">Barcelona</strong> in search of a{" "}
+            <strong className="Name">software engineering role.</strong>
           </p>
         </div>
       </div>
