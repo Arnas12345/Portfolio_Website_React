790d5585b1935a24ab7fdca4673491e6dcb200f8
Arnas12345 <arnasjuravicius12345@gmail.com>
2022-04-12 11:55:44 +0100

    Added Bootstrap and Finished Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,41 @@ export default function Home() {
   return (
     <div className="Page">
       <Particle />
-      <div className="about2">
-        <h1 style={{ color: "white" }}>Home</h1>
+      <div className="row IntoText" style={{ height: "95vh", marginRight: 0 }}>
+        <div className="MainText col-md-7">
+          <h1 className="Intro">Hi There.</h1>
+          <h1 className="Intro">
+            I'm <strong className="Name">Arnas Juravicius.</strong>
+          </h1>
+          <h1 className="Intro">SOFTWARE ENGINEER</h1>
+        </div>
+        <div className="col-md-5">
+          <img
+            src={require("../Images/man-at-desk.png")}
+            className="ManAtDesk"
+            alt="Logo"
+          />
+        </div>
       </div>
-      <div className="MainText">
-        <p className="Intro">Hi There.</p>
-        <p className="Intro">
-          I'm <strong>Arnas Juravicius.</strong>
-        </p>
-        <p className="Jobs">
-          | React/React-Native Dev | MEAN STACK | Problem Solver |
-        </p>
-      </div>
-      <div className="Intro">
-        <h1>About me</h1>
+      <div>
+        <h1 className="Intro">
+          A LITTLE <strong className="Name">ABOUT ME</strong>
+        </h1>
+        <div className="AboutMeText">
+          <p className="AboutMeFont">
+            I am a recent graduate from the University of Limerick where I
+            obtained a First Class Honors in a Bachelor of Science in Computer
+            Systems.
+          </p>
+          <p className="AboutMeFont">
+            I am of Lithuanian descent and have spent all my life in Ireland
+            therefore I am fluent in both English and Lithuanian.
+          </p>
+          <p className="AboutMeFont" style={{ marginBottom: 50 }}>
+            I am currently seeking to relocate to Barcelona in search of a
+            software engineering role.
+          </p>
+        </div>
       </div>
     </div>
   );
